233d8d60d92255259ba87219f96e9c03fa6e9622
maheshwari <maheshwarilodangi.marolix@gmail.com>
2023-11-20 17:53:05 +0530

    mahi first commit

---
+++ assignment gmail.py
@@ -0,0 +1,7 @@
+name=''
+password=''
+while name !='mahi':
+    name=input('enter your name:')
+while password !='7981556461':
+    password=input('enter password:')
+print('thanks you are successfully logd in')
