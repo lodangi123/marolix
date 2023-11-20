ecf7f66fc8a10cd7309ecc8e7268c460fba5ba47
maheshwari <maheshwarilodangi.marolix@gmail.com>
2023-11-20 17:59:18 +0530

    first commit

---
+++ 16th_nov.py
@@ -0,0 +1,22 @@
+
+
+'''s="Aman is good"
+list=s.split(' ')
+print(list)
+list.reverse()
+print(list)
+
+
+
+s= "ABCCCDDDEFGGGHHHIIJJJKLLABCDEF"
+p = ""
+for i in s:
+    if i not in p:
+        p = p+i
+print(p)
+k = list("ABCCCDDDEFGGGHHHIIJJJKLLABCDEF")'''
+
+
+l=[10,20,30,[40,50,60],70,80,90]
+print(l[0],l[1],l[2],l[4],l[5],l[6])
+print(l[3])
---
+++ while loop assignment.py
@@ -0,0 +1,33 @@
+#1write a program to perform sum of n numbers
+'''n=10
+i=1
+sum=0
+while(i<=n):
+  sum+=i 
+  i+=1 
+print("sum of n numbers:",sum)
+z=20
+print(i+z)'''
+
+#2 #write a program to display natural numbers from 1 to 20 using while loop
+'''i=1
+n=20
+while(i<=n):
+  print(i,end=" ")
+  i=i+1'''
+
+#3 write a python program to print welcome message 10 times using while loop
+'''i=1
+n=20
+while(i<=n):
+  print("welcome")
+  i=i+1'''
+
+'''number=10
+x=1
+while x<number:
+    print(x)
+    x=x+1'''
+
+
+
