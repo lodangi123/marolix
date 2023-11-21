81b120872bac742dfbd138133f6305d6fb0b998c
maheshwari <maheshwarilodangi.marolix@gmail.com>
2023-11-21 15:39:07 +0530

    nov assign

---
+++ 20th nov assignments.py
@@ -0,0 +1,81 @@
+'''l=[]
+for i in range(32):
+    if i%2!=0:
+        l.append(i)
+print(l)
+
+l=[]
+for i in range(20):
+    if i%2==0:
+        l.append(i)
+print(l)   
+
+l=[]
+for i in range(10):
+    if i<80:
+        l.append(i)
+print(l)  
+
+a=['mango','banana','cucumber']
+a.append('sorakaya')
+print(a)'''
+
+l= ['1','2','3','4','5','6']
+a=len(l)
+for i in range(a):
+    print(l[i],'is available at +ve index is',i,'and at -ve index is',i-a)
+#l.append('10')
+#print(l)
+#print(l.index('4'))
+#print(l.count(3))
+
+
+'''l=['mango','ramu','mahi','ego']
+a=len(l)
+for i in range(a):
+    print(l[i],'available at +ve index is',i,'and at -ve index is',i-a)
+#print(l.index('mahi'))
+#print(l.count('ramu'))
+
+#a=len(l)
+#for i in range(a):
+ #   print(l[i],"is available +ve index",i,"and at -ve index",i-a)
+
+l.insert(2,'haha')    
+print(l)
+
+
+s=('nanu', 'manishini', 'kanu', 'ani', 'anukuntunava', 'hahaha')
+s=list(s)
+a=len(s)
+for i in range(a):
+    print(s[i],"is available at +ve index is",i,"and at -ve index is",i-a)
+print(a)
+print(a.count('hahaha'))
+print(a.index('ani'))
+a.insert(2,'kaki')
+print(a)
+
+l=('mahi','2','kaki','pitta','10','2')
+#l.insert(3,'haha')
+#print(len(l))
+#print(l.index('2'))
+#print(l.count('2'))
+a=len(l)
+for i in range(a):
+    print(l[i],"available at +ve index is",i,"and at -ve index is",i-a)
+
+s=('iloveyou')
+s=list(s)
+a=len(s)
+for i in range(a):
+    print(s[i],"is available at +ve index is",i,"and at -ve index is",i-a)
+#print(s)
+#print(s.insert(7,'9'))
+#print(s.count('o'))
+#print(s.index('u'))'''
+
+
+
+
+     
