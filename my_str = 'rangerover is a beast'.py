f530cc679108e30b5c64514b239f4d6a7ff30d8d
maheshwari <maheshwarilodangi.marolix@gmail.com>
2023-11-29 12:01:23 +0530

    string, range assign

---
+++ my_str = 'rangerover is a beast'.py
@@ -0,0 +1,33 @@
+'''m = 'rangerover is a beast'
+
+my_list = m.split(' ')
+print(my_list)  
+my_list.reverse()
+print(my_list) ''' 
+
+'''s="python is simple language"
+print(s.upper())'''
+
+#s="Python Is Simple Language"
+#print(s.lower())
+
+#s="Python Is Simple Language"
+#print(s.swapcase())
+
+#s="Python Is Simple Language"
+#print(s.title())
+
+#s="python is simple language"
+#print(s.capitalize())
+
+#s1 = "rangerover is a beast"
+#s2 = "rangerover is a beast"
+#print ("s1[0:21:1]:",s1[0:21:1])
+#print ("s2[0:21]:",s2[0:21])
+s="rangerover is a beast"
+a=s.split()
+for i in a[::-1]:
+    print(i,end="")
+
+
+
---
+++ r=range(100).py
@@ -0,0 +1,119 @@
+#r=range(101)
+#for i in r:
+ #   print(i)
+#r=range(0,101,2)
+#for i in r:
+ #   print(i)
+
+
+#x=10
+#y=30
+#print("x+y :", x+y)
+
+#a=2
+#c=6
+#print(c-a,a-c)
+
+#x="mahi"
+#y=10
+#print(x+y)  TypeError: can only concatenate str (not "int") to str
+
+#m=11
+#v=13
+#print(m*v,m**v)
+
+#x="mahi"
+#y=20
+#print(x*y)
+
+#a=20
+#b=40
+#print(a/b)
+
+#a=2.9
+#b=4.0
+#print(a/b)
+
+#c="vamshi"
+#d="mahi"
+#print(c/d)TypeError: unsupported operand type(s) for /: 'str' and 'str'
+
+#c="vamshi"
+#d=10
+#print(c/d)SyntaxError: invalid syntax
+
+#a=False
+#b=1
+#print(a/b)
+
+#a=True
+#b=1
+#print(a//b)
+
+#a=20
+#b=20.8
+#print(a%b,a+b,a//b,b-a,b==a)
+
+#l="mahi"
+#y="ram"
+#print(l!=y)
+
+#a="mahi"
+#b=30
+#print(a>b,b>a,a<b) TypeError: '>' not supported between instances of 'str' and 'int'
+
+#y="mahi"
+#x=2.0
+#print(y==x,x!=y)
+
+#a=10
+#b=20
+#print(a>=b)
+#print(a<=b)
+
+#x="Mahi"
+#y="mahi"
+#print("a>b",a>b)
+#print(a<b)
+
+#a="ani"
+#b="avi"
+#print(a==b)
+
+#x="money make man imperfect"
+#y="a man can make a women happy"
+#print(x>y)
+#print(x<y)
+
+#a=10
+#b=2
+#print(a>=b,a<=b,a>b,a<b)
+
+#x="you can make people laugh"
+#y=10
+#print(x>y,x<y,x<+y,x>=y)TypeError: '>' not supported between instances of 'str' and 'int'
+
+#print(10!=20)
+#print("ramaraju">"Ram")
+
+#a=10
+#a&=14
+#print(a)
+
+list=(10,30,50,60)
+t=(20,30,40)
+s=(30,2,10,70)
+print(list,sep="&")
+print(t,end="-")
+print(s)
+
+a="mahi"
+b=10
+c=10.0
+print("hello {} can drink {} in just {} mint".format(a,b,c))
+
+
+
+
+
+
