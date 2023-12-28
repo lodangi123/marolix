1461bd935576899432a8dbf78f7057c2e41f98e5
maheshwari <maheshwarilodangi.marolix@gmail.com>
2023-12-28 14:29:46 +0530

    27th assign

---
+++ student_file.py
@@ -0,0 +1,25 @@
+# import csv 
+# with open("Studetnf.csv",'w',newline='') as a:
+#     w=csv.writer(a)
+#     w.writerow(['student_name','student_ID','maths','english','sicence','physics','total','avg',])
+#     n=int(input("entr number of students"))
+#     for i in range(1,n+1):
+#         print(i,'STUDENT DETAILS')
+#         sid=int(input('enter student id:'))
+#         sname=(input("enter studetn name:"))
+#         maths=int(input('entr marks:'))
+#         english=int(input('enter marks:'))
+#         sicence=int(input('enter marks:'))
+#         physics=int(input('enter marks:')) 
+#         a=(maths+english+sicence+physics)  
+#         total=a
+#         y=a/5
+#         avg=y
+#         z=a/500*100
+#         per=z
+#         result="pass"if per>=35 else"fail"
+#         w.writerow([sid,sname,maths,english,sicence,physics,total,avg,per,result])
+#     print('total students data writen to csv file')   
+
+
+
